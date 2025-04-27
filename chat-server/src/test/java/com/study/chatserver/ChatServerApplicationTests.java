5288e82d7a643672875d0f4a8204591023b8d2e8
Kang Duoh <kduoh99@gmail.com>
2025-04-28 03:58:27 +0900

    Chore: 백엔드 프로젝트 초기 세팅 (#1)

---
+++ chat-server/src/test/java/com/study/chatserver/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.chatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
