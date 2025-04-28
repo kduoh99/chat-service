f250274f9d6b96e948c346c052f19364fee0c3d1
Kang Duoh <kduoh99@gmail.com>
2025-04-28 13:04:20 +0900

    Feat: 회원 도메인 설계 (#2)

---
+++ chat-server/src/main/java/com/study/chatserver/member/domain/Member.java
@@ -0,0 +1,42 @@
+package com.study.chatserver.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "member_id")
+	private Long id;
+
+	private String name;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	private String password;
+
+	@Enumerated(EnumType.STRING)
+	private Role role;
+
+	@Builder
+	private Member(String name, String email, String password, Role role) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+}
---
+++ chat-server/src/main/java/com/study/chatserver/member/domain/Role.java
@@ -0,0 +1,5 @@
+package com.study.chatserver.member.domain;
+
+public enum Role {
+	ROLE_ADMIN, ROLE_USER
+}
