8dd803935970d8d498fb13fbfd3656afdc439ef9
Kang Duoh <kduoh99@gmail.com>
2025-04-29 18:23:41 +0900

    Feat: 회원목록 화면 구성 (#2)

--- chat-client/src/main.js
+++ chat-client/src/main.js
@@ -2,9 +2,22 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import vutify from './plugins/vuetify';
 import router from '@/router/index.js';
-// import axios from 'axios';
+import axios from 'axios';
 
 const app = createApp(App);
+
+axios.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 app.use(vutify);
 app.use(router);
