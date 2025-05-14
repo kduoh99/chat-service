164fdd50b8c0579d0a2cbc1cfd4c4b286da1135e
Kang Duoh <kduoh99@gmail.com>
2025-05-14 18:03:42 +0900

    Feat: RequestLoggingFilter 등록 (#7)

---
+++ chat-server/src/main/java/com/study/chatserver/global/config/FilterConfig.java
@@ -0,0 +1,20 @@
+package com.study.chatserver.global.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.study.chatserver.global.logging.RequestLoggingFilter;
+
+@Configuration
+public class FilterConfig {
+
+	@Bean
+	public FilterRegistrationBean<RequestLoggingFilter> requestLoggingFilter() {
+		FilterRegistrationBean<RequestLoggingFilter> registration = new FilterRegistrationBean<>();
+		registration.setFilter(new RequestLoggingFilter());
+		registration.setOrder(1);
+		registration.addUrlPatterns("/*");
+		return registration;
+	}
+}
